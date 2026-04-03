eae68adeb125ab5a16a500c9ca4025d3bc50b147
Edson Ney de Farias <edson.ney@gmail.com>
2026-04-03 15:46:47 -0300

    First commit

---
+++ src/test/java/com/devsuperior/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
